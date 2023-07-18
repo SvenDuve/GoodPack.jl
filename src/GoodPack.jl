d1b29b1217c6419ac3fd4d87c037d945d01e61bf
Sven Duve <svenduve@gmail.com>
2023-07-18 20:28:20 +0200

    init

--- src/GoodPack.jl
+++ src/GoodPack.jl
@@ -1,5 +1,14 @@
 module GoodPack
 
 # Write your package code here.
+export addSome
+
+"""
+    addSome(x, y)
+
+Add two numbers.
+"""
+addSome(x, y) = x + y
+
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,5 @@ using Test
 
 @testset "GoodPack.jl" begin
     # Write your tests here.
+    @test addSome(1, 2) == 3
 end
